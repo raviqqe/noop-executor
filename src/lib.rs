30bf08c13b00e1a5cd206511d5d9114e764133b6
Yota Toyama <raviqqe@gmail.com>
2025-04-24 09:13:50 +0900

    `block_on` (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
-pub fn foo() {
-    println!("Hello, world!");
+//! No-op executors.
+
+#![no_std]
+
+use core::{
+    pin::pin,
+    task::{Context, Poll, Waker},
+};
+
+/// Blocks on a future.
+///
+/// It panics if a future is not ready.
+pub fn block_on<T>(future: impl Future<Output = T>) -> T {
+    let Poll::Ready(result) = pin!(future).poll(&mut Context::from_waker(Waker::noop())) else {
+        unreachable!()
+    };
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use core::future::{pending, ready};
+
+    #[test]
+    fn resolve_ready_future() {
+        assert_eq!(block_on(async { 42 }), 42);
+        assert_eq!(block_on(ready(42)), 42);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_on_pending_future() {
+        assert_eq!(block_on(pending::<usize>()), 42);
+    }
 }
