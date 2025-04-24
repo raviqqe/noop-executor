d403ba63b5ba9ca232edb4534751de6e67535dd8
Yota Toyama <raviqqe@gmail.com>
2025-04-23 17:04:34 -0700

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
-pub fn foo() {
-    println!("Hello, world!");
+//! No-op executors.
+
+#![no_std]
+
+use core::{
+    pin::pin,
+    task::{Context, Poll, Waker},
+};
+
+/// Blocks on a future.
+pub fn block_on<T>(future: impl Future<Output = T>) -> T {
+    let Poll::Ready(result) = pin!(future).poll(&mut Context::from_waker(Waker::noop())) else {
+        unreachable!()
+    };
+
+    result
 }
